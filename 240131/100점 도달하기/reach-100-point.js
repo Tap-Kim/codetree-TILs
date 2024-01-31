f88c0993adeee4c17fcf5e354db187ee79beaec2
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-01-31 22:08:22 +0900

    Title: 100점 도달하기, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240131/100점 도달하기/reach-100-point.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let result = "";
+
+const len = 100 - n;
+let std = n;
+for(let i = 0; i < len; i++, std++) {
+    if(std >= 90) result += 'A ';
+    else if(std >= 80 && std < 90) result += 'B ';
+    else if(std >= 70 && std < 80) result += 'C ';
+    else if(std >= 60 && std < 70) result += 'D ';
+    else if(std < 60) result += 'F ';
+}
+
+console.log(result);
