c91613b7d7be3bac6ba837e534c0f3e8625636fc
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-01-31 22:09:43 +0900

    Title: 100점 도달하기, Time: 222ms, Memory: 14MB, Status: Passed - CodeTree

--- 240131/100점 도달하기/reach-100-point.js
+++ 240131/100점 도달하기/reach-100-point.js
@@ -4,7 +4,7 @@ let result = "";
 
 const len = 100 - n;
 let std = n;
-for(let i = 0; i < len; i++, std++) {
+for(let i = 0; i <= len; i++, std++) {
     if(std >= 90) result += 'A ';
     else if(std >= 80 && std < 90) result += 'B ';
     else if(std >= 70 && std < 80) result += 'C ';
