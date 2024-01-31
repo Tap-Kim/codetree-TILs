a8e5e09c6df36257ed46bccf4054c01b223e231f
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-01-31 22:36:15 +0900

    Title: 369 게임, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

--- 240131/369 게임/369-game.js
+++ 240131/369 게임/369-game.js
@@ -6,9 +6,9 @@ let result = '';
 for(let i = 1; i <= n; i++) {
     if(i % 3 === 0) result += '0 ';
     else if(
-        String(i).split('').indexOf('3') > 0 
-        || String(i).split('').indexOf('6') > 0 
-        || String(i).split('').indexOf('9') > 0
+        String(i).indexOf('3') > -1
+        || String(i).indexOf('6') > -1
+        || String(i).indexOf('9') > -1
         ) result += '0 ';
     else result += i + ' ';
 }
