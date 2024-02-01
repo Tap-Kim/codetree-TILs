b2b67a4a4a2e174e39de8a8184cabebbb02b3110
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-01 23:12:26 +0900

    Title: 배수의 개수, Time: 317ms, Memory: 14MB, Status: Passed - CodeTree

--- 240201/배수의 개수/number-of-multipliers.js
+++ 240201/배수의 개수/number-of-multipliers.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const lines = fs.readFileSync(0).toString().split("\n");
 
-let a = 0, b = 0;
+let cntList = [0, 0];
 
-for(let i = 0; i< lines.length; i++) {
-    if(Number(lines[i]) % 3 === 0) a++;
-    if(Number(lines[i]) % 5 === 0) b++;
+for(let i = 0; i < lines.length; i++) {
+    if(lines[i].trim() === "") continue; 
+    if(Number(lines[i]) % 3 === 0) cntList[0]++;
+    if(Number(lines[i]) % 5 === 0) cntList[1]++;
 }
 
-console.log(a, b)
+console.log(cntList[0], cntList[1])
