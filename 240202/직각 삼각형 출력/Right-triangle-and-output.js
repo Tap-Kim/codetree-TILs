2ca0752b6523ef1886c68a8637dcbebb24aa30c1
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-02 23:26:28 +0900

    Title: 직각 삼각형 출력, Time: 228ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 240202/직각 삼각형 출력/Right-triangle-and-output.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let result = '';
+
+for(let i = 0; i < n; i++) {
+    for(let j = 0; j <= i * 2; j++) {
+        result += '*';
+    }
+	result += '\n';
+}
+
+console.log(result)
