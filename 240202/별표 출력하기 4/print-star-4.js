12988cc19e0377d51ef193301fcdb0c44cd342ef
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-02 23:51:36 +0900

    Title: 별표 출력하기 4, Time: 220ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240202/별표 출력하기 4/print-star-4.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let result = '';
+
+for(let i = 0; i < n; i++) {
+    for(let j = n; j > i; j--) {
+        result += '* ';
+    }
+	result += '\n';
+}
+for(let i = 1; i < n; i++) {
+    for(let j = 0; j < i + 1; j++) {
+        result += '* ';
+    }
+	result += '\n';
+}
+console.log(result)
