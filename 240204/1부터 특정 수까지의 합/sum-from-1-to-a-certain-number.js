4990c585417de28b9066f0072a98baeae219e605
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-04 17:00:42 +0900

    Title: 1부터 특정 수까지의 합, Time: 214ms, Memory: 14MB, Status: Passed - CodeTree

--- 240204/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
+++ 240204/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
@@ -1,5 +1,5 @@
 function sum(num) {
-    return (num * (num + 1) / 2) / 10
+    return Math.floor((num * (num + 1) / 2) / 10)
 }
 const fs = require("fs"); 
 let num = Number(fs.readFileSync(0).toString().trim()); 
