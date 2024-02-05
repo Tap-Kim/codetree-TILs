0dac15418d1aca5f11dae04dfb0dc03dfa942798
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-05 23:55:47 +0900

    Title: 함수를 이용한 윤년 판별, Time: 211ms, Memory: 14MB, Status: Passed - CodeTree

--- 240205/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
+++ 240205/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
@@ -1,14 +1,14 @@
 function isYun(n) {
-    if (n % 4 === 0 && n % 100 === 0 && n % 400 === 0) {
+    if (n % 400 === 0) {
         return true;
     }
-    if (n % 4 === 0 && n % 100 === 0) {
+    if (n % 100 === 0) {
         return false;
     }
     if (n % 4 === 0) {
         return true;
     }
-    return true;
+    return false;
 }
 
 const fs = require("fs"); 
