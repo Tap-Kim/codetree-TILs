786f5d01ce9a64e913d521b4988aa9eb19f91aa8
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-07 01:01:30 +0900

    Title: 그 계절, 그 날, Time: 200ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240207/그 계절, 그 날/that-season-that-day.js
+++ 240207/그 계절, 그 날/that-season-that-day.js
@@ -1,14 +1,21 @@
 function isYun(year) {
-    if(year % 400 !== 0) {
-        return false;
-    } 
-    if(year % 100 === 0){
-        return false;
-    } 
-    if(year % 4 !== 0){
-        return false;
-    }
-    return true;
+    // if(year % 400 !== 0) {
+    //     return false;
+    // } 
+    // if(year % 100 === 0){
+    //     return false;
+    // } 
+    // if(year % 4 !== 0){
+    //     return false;
+    // }
+    
+    return (
+            year % 4 === 0 && 
+                (
+                    (year % 100 !== 0) || 
+                    ((year % 100 === 0 && year % 400 === 0))
+                )
+            )
 }
 
 function getFourSeaon(year, month, date) {
