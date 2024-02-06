28007ec244f3afa4f4c545a60ea05e366daf4e2f
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-07 00:39:19 +0900

    Title: 2021년 날짜의 유무, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240207/2021년 날짜의 유무/with-or-without-2021.js
@@ -0,0 +1,27 @@
+function getAnswer(isBool) {
+    return isBool ? 'Yes' : 'No';
+}
+
+function getResultMonthDate(M,D) {
+    let result = 'No';
+    switch(M){
+        case '2':
+            result = getAnswer(D >= 1 && D <= 28);
+            break;
+        // 31까지
+        case '1':case '3':case '5':case '7':case '8':case '10':case '12':
+            result = getAnswer(D >= 1 && D <= 31);
+            break;
+        // 30일 까지
+        default:
+            result = getAnswer(D >= 1 && D <= 30);
+            break;
+    }
+    return result;
+}
+
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let [M, D] = input.map(Number);
+
+console.log(getResultMonthDate(M,D))
