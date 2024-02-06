3096179d54206751e29936ed5c02afc0a72236a2
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-07 00:55:09 +0900

    Title: 그 계절, 그 날, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240207/그 계절, 그 날/that-season-that-day.js
@@ -0,0 +1,34 @@
+function isYun(year) {
+    if(year % 400 !== 0) {
+        return false;
+    } else if(year % 100 === 0){
+        return false;
+    } else if(year % 4 !== 0){
+        return false;
+    }
+    return true;
+}
+
+function getFourSeaon(year, month, date) {
+    if(month >= 3 && month <= 5){
+        return 'Spring';
+    } else if(month >= 6 && month <= 8){
+        return 'Summer';
+    } else if(month >= 9 && month <= 11){
+        return 'Fall';
+    } else if(month >= 12 || [1, 2].includes(month)){
+        if(isYun(year)) {
+            return date > 29 ? -1 : 'Winter';
+        } else {
+            return 'Winter';
+        }
+    } else {
+        return -1;
+    }
+}
+
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let [Y,M,D] = input.map(Number);
+
+console.log(getFourSeaon(Y,M,D));
