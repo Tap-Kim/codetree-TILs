f0ffe49ef13649f8b76e0a1dbbd6f52ec05f6db3
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-07 01:26:16 +0900

    Title: 그 계절, 그 날, Time: 316ms, Memory: 14MB, Status: Passed - CodeTree

--- 240207/그 계절, 그 날/that-season-that-day.js
+++ 240207/그 계절, 그 날/that-season-that-day.js
@@ -1,11 +1,5 @@
 function isYun(year) {
-    return (
-            year % 4 === 0 && 
-                (
-                    (year % 100 !== 0) || 
-                    ((year % 100 === 0 || year % 400 === 0))
-                )
-            )
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)
 }
 
 function isExistDate(year, month, date) { 
