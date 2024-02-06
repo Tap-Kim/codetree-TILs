60c1a21c1a4c452d74632de899315ab85c4342b7
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-07 00:58:15 +0900

    Title: 그 계절, 그 날, Time: 229ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240207/그 계절, 그 날/that-season-that-day.js
+++ 240207/그 계절, 그 날/that-season-that-day.js
@@ -1,9 +1,11 @@
 function isYun(year) {
     if(year % 400 !== 0) {
         return false;
-    } else if(year % 100 === 0){
+    } 
+    if(year % 100 === 0){
         return false;
-    } else if(year % 4 !== 0){
+    } 
+    if(year % 4 !== 0){
         return false;
     }
     return true;
