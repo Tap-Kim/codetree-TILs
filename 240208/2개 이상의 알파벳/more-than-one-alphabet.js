8bcef19ef788f7cae8afbdfec130def1d69e402b
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-08 21:14:12 +0900

    Title: 2개 이상의 알파벳, Time: 194ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240208/2개 이상의 알파벳/more-than-one-alphabet.js
+++ 240208/2개 이상의 알파벳/more-than-one-alphabet.js
@@ -1,15 +1,29 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString()
 
-const map = new Map()
+const map = {}
 
 for(let i = 0; i < input.length; i++) {
     const key = input[i]
-    if(map.has(key)) {
-        map.set(key, map.get(key) + 1);
+    if(map?.[key]) {
+        map[key] = map[key] + 1;
     } else {
-        map.set(key, 1)
+        map[key] = 1;
     }
 }
 
-console.log(map.size > 1 ? 'Yes' : 'No')
+console.log(Object.values(map).length > 1 ? 'Yes' : 'No')
+// console.log(map.size > 1 ? 'Yes' : 'No')
+
+// const map = new Map()
+
+// for(let i = 0; i < input.length; i++) {
+//     const key = input[i]
+//     if(map.has(key)) {
+//         map.set(key, map.get(key) + 1);
+//     } else {
+//         map.set(key, 1)
+//     }
+// }
+
+// console.log(map.size > 1 ? 'Yes' : 'No')
