0a4d11f2ace03ed44166e553fd3452059435a950
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-11 01:09:19 +0900

    Title: 재귀함수를 이용한 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240211/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
+++ 240211/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
@@ -1,9 +1,10 @@
-function f(arr, n) {
+const fs = require("fs");
+let [n, _arr] = fs.readFileSync(0).toString().trim().split("\n");
+const arr = _arr.trim().split(' ').map(Number)
+
+function f(n) {
     if(n === 0) return arr[0];
-    return Math.max(f(arr, n - 1), arr[0])
+    return Math.max(f(n - 1), arr[a])
 }
 
-const fs = require("fs");
-let [n, arr] = fs.readFileSync(0).toString().trim().split("\n");
-
-console.log(f(arr.trim().split(' ').map(Number), n - 1))
+console.log(f(n - 1))
