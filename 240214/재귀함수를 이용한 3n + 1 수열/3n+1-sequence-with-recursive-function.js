7dfb3ee0ba8e3fada806ba19909c5fab6d9e345f
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-14 23:45:32 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 239ms, Memory: 15MB, Status: Passed - CodeTree

--- 240214/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.js
+++ 240214/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.js
@@ -3,8 +3,8 @@ let num = Number(fs.readFileSync(0).toString().trim());
 
 let cnt = 0;
 function f(n) {
-    cnt++;
     if (n === 1) return 1;
+    cnt++;
     if (n === 2) return 2;
     return f(n % 2 === 0 ? parseInt(n/2) : (n * 3) + 1)
 }
