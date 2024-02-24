ed726b1b3637e3f9ebd89b16150f6b5af4d025d2
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-24 21:13:07 +0900

    Title: Top K 숫자 구하기, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

--- 240224/Top K 숫자 구하기/kth-number.js
+++ 240224/Top K 숫자 구하기/kth-number.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let [a, b] = fs.readFileSync(0).toString().trim().split("\n");
 const [, k] = a.trim().split(' ');
-const arr = b.trim().split(' ').sort();
+const arr = b.trim().split(' ').sort((a, b) => a - b);
 
 console.log(arr[k - 1])
