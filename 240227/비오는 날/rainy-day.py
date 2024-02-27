c3122b554e34adee4cc82107b87a5eaa1c715e46
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-27 22:58:45 +0900

    Title: 비오는 날, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240227/비오는 날/rainy-day.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+class Forecast:
+    def __init__(self, date, day, weather):
+        self.date, self.day, self.weather = date, day, weather
+
+ans = Forecast("9999-99-99", "", "")
+for _ in range(n):
+    date, day, weather = tuple(input().split())
+
+    f = Forecast(date, day, weather)
+    if(weather == 'Rain'):
+        if(ans.date >= f.date):
+            ans = f
+
+print(f"{ans.date} {ans.day} {ans.weather}")
