d1ecad827e8d3d587ecb653425949de85d1d53db
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-29 19:40:43 +0900

    Title: 개인정보, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

--- 240229/개인정보/personal-info.js
+++ 240229/개인정보/personal-info.js
@@ -12,7 +12,7 @@ let inputs = fs.readFileSync(0).toString().trim().split("\n");
 const persons = []
 for(const p of inputs) {
     const person = p.trim().split(" ")
-    persons.push(new Person(person[0], Number(person[1]), Number(person[2])))
+    persons.push(new Person(person[0], person[1], person[2]))
 }
 
 persons.sort((a,b) => b.n > a.n ? -1 : 1)
