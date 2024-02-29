d5e123f9b46348be94a864d9d1711b3bcf506e7e
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-02-29 21:39:13 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 225ms, Memory: 47MB, Status: Passed - CodeTree

--- 240229/정렬된 숫자 위치 알아내기/indices-of-sorted-array.js
+++ 240229/정렬된 숫자 위치 알아내기/indices-of-sorted-array.js
@@ -1,14 +1,13 @@
 const fs = require("fs");
 let [n, data] = fs.readFileSync(0).toString().trim().split("\n");
 const inputs = data.trim().split(" ").map(Number);
+
 let originList = inputs.map((num, index) => ({num: Number(num), index}))
-
+const answer = Array.from({length: n}).fill(0)
 originList.sort((a, b) => a.num - b.num || a.index - b.index);
-originList = originList.map((origin, i) => ({...origin, orderedIndex: i}));
-originList.sort((a,b) => a.index - b.index)
 
-const list = []
 for(let i = 0; i < n; i++) {
-    list.push(originList[i].orderedIndex + 1)
+    answer[originList[i].index] = i + 1;
 }
-console.log(list.join(' '))
+
+console.log(answer.join(' '))
