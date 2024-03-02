5d6c2aec808fcc793d2338f954a31d9631bcd77b
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-03-02 14:33:10 +0900

    Title: 그 요일은, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240302/그 요일은/the-day-of-the-day.js
@@ -0,0 +1,39 @@
+//                 1.  2.  3.  4.  5.  6.  7.  8.  9.  10. 11. 12.
+const MONTHS = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+const fs = require("fs");
+let [dates, _day] = fs.readFileSync(0).toString().trim().split("\n");
+const day = _day.trim()
+const [m1, d1, m2, d2] = dates.trim().split(" ").map(Number);
+// m1, d1 => Mon
+// 2월 5일 => Mon
+
+// if (m1 === m2) {
+//     console.log(d2 - d1 + 1)
+//     return;
+// }
+
+let totalDays = 1;
+
+for (let month = m1; month < m2; month++) {
+    totalDays += MONTHS[month];
+}
+totalDays -= d1;
+totalDays += d2;
+
+function f(n) {
+    if(DAYS[n] === day) {
+        return true}
+    else if(n % 7 === n) return false
+    else return f(n % 7)
+}
+
+let cnt = 0;
+for(let i = totalDays; 0 < i; i--) {
+    if(f(i)) {
+        cnt++;
+    }
+}
+
+console.log(cnt)
