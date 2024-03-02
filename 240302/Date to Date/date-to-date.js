0d09c3f0469e564203bca610b434c5a32a28e1ec
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-03-02 11:48:47 +0900

    Title: Date to Date, Time: 189ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240302/Date to Date/date-to-date.js
+++ 240302/Date to Date/date-to-date.js
@@ -5,17 +5,32 @@ const NUM_OF_DAYS = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const fs = require("fs");
 const [m1, d1, m2, d2] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-let daySum = 1;
-if(d1 === d2 && m1 === m2) {
-    console.log(daySum)
-    return;
+if (m1 === m2) {
+    return d2 - d1 + 1;
 }
 
-for(let i = m1; i <= m2; i++) {
-    daySum += NUM_OF_DAYS[i - 1];
+let totalDays = NUM_OF_DAYS[m1 - 1] - d1;
+
+for (let month = m1; month < m2 - 1; month++) {
+    totalDays += NUM_OF_DAYS[month];
 }
 
-daySum -= d1;
-daySum += d2;
+totalDays += d2;
 
-console.log(daySum)
+console.log(totalDays)
+
+
+// let daySum = 1;
+// if(d1 === d2 && m1 === m2) {
+//     console.log(daySum)
+//     return;
+// }
+
+// for(let i = m1; i <= m2; i++) {
+//     daySum += NUM_OF_DAYS[i - 1];
+// }
+
+// daySum -= d1;
+// daySum += d2;
+
+// console.log(daySum)
