8b4b8578192f4590f52069b4801654a96a73cc47
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-03-02 11:36:13 +0900

    Title: Date to Date, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240302/Date to Date/date-to-date.js
+++ 240302/Date to Date/date-to-date.js
@@ -5,10 +5,12 @@ const NUM_OF_DAYS = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const fs = require("fs");
 const [m1, d1, m2, d2] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-let daySum = 0;
+let daySum = 1;
 for(let i = m1; i <= m2; i++) {
-    daySum += NUM_OF_DAYS[i];
+    daySum += NUM_OF_DAYS[i - 1];
 }
 
-daySum = daySum - NUM_OF_DAYS[m1] + (m1 !== m2 ? (d1 + NUM_OF_DAYS[m2] - d2) : 0) + 1;
+daySum -= d1;
+daySum += d2;
+
 console.log(daySum)
