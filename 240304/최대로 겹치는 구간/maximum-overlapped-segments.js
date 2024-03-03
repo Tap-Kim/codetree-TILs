f16a404d7d777380c04d361c5fa0bbf5d5b07e4f
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-03-04 00:53:32 +0900

    Title: 최대로 겹치는 구간, Time: 176ms, Memory: 14MB, Status: Passed - CodeTree

--- 240304/최대로 겹치는 구간/maximum-overlapped-segments.js
+++ 240304/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -1,12 +1,13 @@
+const OFFESET = 100
 const fs = require('fs');
 const [n, ...inputs] = fs.readFileSync(0).toString().trim().split("\n");
 
-const arr = Array.from({length: 200}).fill(0);
+const arr = Array.from({length: 201}).fill(0);
 
 for(let i = 0; i < n; i++) {
-    const [a, b] = inputs[i].trim().split(" ");
-    for(let j = a; j < b - 1; j++) {
-        arr[j]++
+    const [a, b] = inputs[i].trim().split(" ").map(Number);
+    for(let j = a; j < b; j++) {
+        arr[j + OFFESET]++;
     }
 }
 
