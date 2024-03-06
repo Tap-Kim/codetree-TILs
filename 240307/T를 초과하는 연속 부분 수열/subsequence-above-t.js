aebe253bbe49b63eba8488eef0ce5f9adafe726e
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-03-07 00:45:10 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 324ms, Memory: 14MB, Status: Passed - CodeTree

--- 240307/T를 초과하는 연속 부분 수열/subsequence-above-t.js
+++ 240307/T를 초과하는 연속 부분 수열/subsequence-above-t.js
@@ -4,17 +4,17 @@ const nums = inputs.toString().trim().split(" ").map(Number);
 
 const result = []
 let arr = [];
+let position = 0
 for(let i = 0; i < n; i++) {
     if(t < nums[i]){
-        position = i;
-        if(i === 0 || nums[i] === arr[arr.length - 1] + 1) {
+        if(i === 0 || i === position + 1) {
             arr.push(nums[i])
-        } else if(nums[i] !== arr[arr.length - 1] + 1) {
-            result.push(arr)
-            arr = [nums[i]]
         } else {
+            result.push(arr)
+            arr = []
             arr.push(nums[i])
         }
+        position = i;
     }
 
     if(i === n - 1) {
