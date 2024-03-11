8cbe10c5a40cfd35acde69a49a93c76eba121bec
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-03-11 09:03:29 +0900

    Title: 선두를 지켜라 3, Time: 182ms, Memory: 41MB, Status: Passed - CodeTree

---
+++ 240311/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,37 @@
+MAX_T = 1000000
+
+n, m = tuple(map(int, input().split()))
+pos_a, pos_b = [0] * (MAX_T + 1), [0] * (MAX_T + 1)
+
+time_a = 1
+for _ in range(n):
+    v, t = tuple(map(int, input().split()))
+    for _ in range(t):
+        pos_a[time_a] = pos_a[time_a - 1] + v
+        time_a += 1
+
+time_b = 1
+for _ in range(m):
+    v, t = tuple(map(int, input().split()))
+    for _ in range(t):
+        pos_b[time_b] = pos_b[time_b - 1] + v
+        time_b += 1
+
+leader, ans = 0, 0
+for i in range(1, time_a):
+    if pos_a[i] > pos_b[i]:
+        if leader != 1:
+            ans += 1
+        leader = 1
+
+    elif pos_a[i] < pos_b[i]:
+        if(leader != 2):
+            ans += 1
+        leader = 2
+
+    else:
+        if(leader != 3):
+            ans += 1
+        leader = 3
+
+print(ans)
