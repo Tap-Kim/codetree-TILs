bd1ebbabd113e38277e8f080cf9c128fa55cebdf
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-03-27 23:19:27 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 214ms, Memory: 47MB, Status: Passed - CodeTree

--- 240327/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.js
+++ 240327/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.js
@@ -8,18 +8,18 @@ for(let i = 0; i < n; i++) {
     const matrixList = matrix[i].toString().trim().split(" ").map(Number);
     answer.push(matrixList)    
 }
-let isEscape = 0;
+
 function dfs(x, y) {
     if(x < 0 || y < 0 || x >= n || y >= m || answer[x][y] === 0 || visited[x][y]) {
         return;
     }
 
-    visited[x][y] = true;
-
     if(x === n - 1 && y === m - 1) {
-        isEscape = 1;
+        visited[x][y] = true;
         return;
     }
+
+    visited[x][y] = true;
 
     dfs(x + 1, y);
     dfs(x, y + 1);
@@ -27,4 +27,4 @@ function dfs(x, y) {
 
 dfs(0,0)
 
-console.log(isEscape)
+console.log(visited[n - 1][m - 1] ? 1 : 0)
