8732de22d8bb56524f67421213108d7dce03e65b
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-03-28 00:37:54 +0900

    Title: 마을 구분하기, Time: 219ms, Memory: 47MB, Status: Passed - CodeTree

--- 240328/마을 구분하기/seperate-village.js
+++ 240328/마을 구분하기/seperate-village.js
@@ -9,27 +9,64 @@ for(let i = 0; i < N; i++){
     matrix.push(row)
 }
 
+// function dfs(x,y) {
+//     if(x < 0 || y < 0 || x >= n || y >= n || matrix[x][y] === 0 || visited[x][y]) {
+//         return 0;
+//     }
+
+//     visited[x][y] = true;
+
+//     let cnt = 1;
+//     cnt += dfs(x + 1,  y);
+//     cnt += dfs(x - 1,  y);
+//     cnt += dfs(x,  y + 1);
+//     cnt += dfs(x,  y - 1);
+
+//     return cnt;
+// }
+
+// const village = [];
+// for(let i = 0; i < n; i++) {
+//     for(let j = 0; j < n; j++) {
+//         if(matrix[i][j] && !visited[i][j]) {
+//             village.push(dfs(i,j))
+//         }
+//     }
+// }
+
+function canGo(x, y) {
+    if(x < 0 || y < 0 || x >= N || y >= N || matrix[x][y] === 0 || visited[x][y]) {
+        return false;
+    }
+    return true;
+}
+
+let cnt = 0;
 function dfs(x,y) {
-    if(x < 0 || y < 0 || x >= n || y >= n || matrix[x][y] === 0 || visited[x][y]) {
-        return 0;
+    if(canGo(x,y)) {
+        return;
     }
 
-    visited[x][y] = true;
-
-    let cnt = 1;
-    cnt += dfs(x + 1,  y);
-    cnt += dfs(x - 1,  y);
-    cnt += dfs(x,  y + 1);
-    cnt += dfs(x,  y - 1);
-
-    return cnt;
+    // 0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽
+    const dxs = [0, 1, 0, -1], dys = [1, 0, -1, 0];
+    for(let dir = 0; dir < 4; dir++) {
+        const newX = x + dxs[dir], newY = y + dys[dir];
+        if(canGo(newX, newY)) {
+            visited[newX][newY] = true;
+            cnt += 1;
+            dfs(newX, newY)
+        }
+    }
 }
 
 const village = [];
-for(let i = 0; i < n; i++) {
-    for(let j = 0; j < n; j++) {
-        if(matrix[i][j] && !visited[i][j]) {
-            village.push(dfs(i,j))
+for(let i = 0; i < N; i++) {
+    for(let j = 0; j < N; j++) {
+        if(canGo(i,j)) {
+            visited[i][j] = true;
+            cnt = 1;
+            dfs(i,j);
+            village.push(cnt);
         }
     }
 }
