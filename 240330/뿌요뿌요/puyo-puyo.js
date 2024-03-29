6a03bccebc607decacffc69a71cf17e519a8ef74
devTap <93532696+Tap-Kim@users.noreply.github.com>
2024-03-30 00:31:59 +0900

    Title: 뿌요뿌요, Time: 222ms, Memory: 18MB, Status: Passed - CodeTree

--- 240330/뿌요뿌요/puyo-puyo.js
+++ 240330/뿌요뿌요/puyo-puyo.js
@@ -13,38 +13,76 @@ for(let i = 0; i < n; i++){
     matrix.push(row)
 }
 
-const dx = [0, 1, 0, -1], dy = [1, 0, -1, 0];
-let maxBlockSize = 0, explondingBlocks = 0;
+let maxBlockSize = 0, bombBlock = 0, count = 0;
 
-for(let i = 0; i < n; i++){
-    for(let j = 0; j < n; j++){
-        if(!visited[i][j]) {
-            const blockSize = dfs(i, j, matrix[i][j]);
-            if(blockSize >= 4) explondingBlocks++;
-            maxBlockSize = Math.max(maxBlockSize, blockSize)
+for(let x = 0; x < n; x++){
+    for(let y = 0; y < n; y++){
+        if(!visited[x][y]) {
+            visited[x][y] = true;
+            count = 1;
+            dfs(x,y);
+
+            if(count >= 4) bombBlock++;
+            maxBlockSize = Math.max(maxBlockSize, count)
         }
-    }   
-}
-console.log(explondingBlocks, maxBlockSize)
-
-function dfs(x, y, cur) {
-    if(canGo(x, y, cur)) return 0;
-    
-    visited[x][y] = true;
-    let count = 1;
-    for(let i = 0; i < 4; i++) {
-        const newX = x + dx[i];
-        const newY = y + dy[i]
-        count += dfs(newX, newY, cur);
     }
+}
 
-    return count;
-} 
+console.log(bombBlock, maxBlockSize)
 
-function isRange(x,y) {
+function dfs(x,y) {
+    const dx = [0, 1, 0, -1], dy = [1, 0, -1, 0];
+    for(let i = 0; i < 4; i++){
+        const newX = x + dx[i], newY = y + dy[i];
+        if(canGo(newX, newY, matrix[x][y])) {
+            visited[newX][newY] = true;
+            count += 1;
+            dfs(newX, newY)
+        }
+    }
+}
+
+function canGo(x, y, cur) {
+    if(!isRange(x,y) || visited[x][y] || matrix[x][y] !== cur) return false
+    return true
+}
+
+function isRange(x, y) {
     return x >= 0 && y >= 0 && x < n && y < n
 }
-function canGo(x, y, cur) {
-    if(!isRange(x,y) || visited[x][y] || matrix[x][y] !== cur) return true;
-    return false;
-}
+
+
+// let maxBlockSize = 0, explondingBlocks = 0;
+
+// for(let i = 0; i < n; i++){
+//     for(let j = 0; j < n; j++){
+//         if(!visited[i][j]) {
+//             const blockSize = dfs(i, j, matrix[i][j]);
+//             if(blockSize >= 4) explondingBlocks++;
+//             maxBlockSize = Math.max(maxBlockSize, blockSize)
+//         }
+//     }   
+// }
+// console.log(explondingBlocks, maxBlockSize)
+
+// function dfs(x, y, cur) {
+//     if(canGo(x, y, cur)) return 0;
+    
+//     visited[x][y] = true;
+//     let count = 1;
+//     for(let i = 0; i < 4; i++) {
+//         const newX = x + dx[i];
+//         const newY = y + dy[i]
+//         count += dfs(newX, newY, cur);
+//     }
+
+//     return count;
+// } 
+
+// function isRange(x,y) {
+//     return x >= 0 && y >= 0 && x < n && y < n
+// }
+// function canGo(x, y, cur) {
+//     if(!isRange(x,y) || visited[x][y] || matrix[x][y] !== cur) return true;
+//     return false;
+// }
